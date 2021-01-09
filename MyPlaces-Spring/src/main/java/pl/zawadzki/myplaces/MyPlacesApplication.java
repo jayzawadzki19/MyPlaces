3975d42a14869769bba89e12d6a517e80d650ac9
Jay <jzawadzki19@gmail.com>
2021-01-10 00:40:46 +0100

    initial backend commit

---
+++ MyPlaces-Spring/src/main/java/pl/zawadzki/myplaces/MyPlacesApplication.java
@@ -0,0 +1,13 @@
+package pl.zawadzki.myplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPlacesApplication.class, args);
+    }
+
+}
---
+++ MyPlaces-Spring/src/test/java/pl/zawadzki/myplaces/MyPlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zawadzki.myplaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
