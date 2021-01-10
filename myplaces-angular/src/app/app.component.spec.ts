679e1a74648cc8ac1d917817091927934dcb3648
Jay <jzawadzki19@gmail.com>
2021-01-10 22:50:47 +0100

    changed project name

--- myplaces-angular/src/app/app.component.spec.ts
+++ myplaces-angular/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'MyPlaces-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyPlaces-Angular');
+    expect(app.title).toEqual('myplaces-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MyPlaces-Angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('myplaces-angular app is running!');
   });
 });
--- myplaces-angular/src/app/app.component.ts
+++ myplaces-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MyPlaces-Angular';
+  title = 'myplaces-angular';
 }
