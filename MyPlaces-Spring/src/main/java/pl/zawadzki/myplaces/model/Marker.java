8b84b2bbab0784ba7b14c2ba0c9c6fd6ef83c4bc
Jay <jzawadzki19@gmail.com>
2021-01-10 01:39:29 +0100

    created User class and updated Markers

--- MyPlaces-Spring/src/main/java/pl/zawadzki/myplaces/model/Marker.java
+++ MyPlaces-Spring/src/main/java/pl/zawadzki/myplaces/model/Marker.java
@@ -4,16 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "markers")
 public class Marker {
 
     @Id
@@ -34,5 +33,18 @@ public class Marker {
     public void setCoordinates(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Marker marker = (Marker) o;
+        return Double.compare(marker.latitude, latitude) == 0 && Double.compare(marker.longitude, longitude) == 0 && id.equals(marker.id) && title.equals(marker.title) && Objects.equals(description, marker.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, latitude, longitude, title, description);
     }
 }
---
+++ MyPlaces-Spring/src/main/java/pl/zawadzki/myplaces/model/User.java
@@ -0,0 +1,40 @@
+package pl.zawadzki.myplaces.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "myplaces_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 5, message = "Password must contain at least 5 characters")
+    private String password;
+
+    @NotEmpty
+    private String role = "ROLE_USER";
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Marker> markers = new HashSet<>();
+
+    
+
+}
