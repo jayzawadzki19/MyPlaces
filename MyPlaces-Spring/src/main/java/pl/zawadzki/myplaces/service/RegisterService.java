830b0cd81be3ceef05f8e3e550abecdafc6f46bf
Jay <jzawadzki19@gmail.com>
2021-01-13 01:56:02 +0100

    created RegisterService

---
+++ MyPlaces-Spring/src/main/java/pl/zawadzki/myplaces/service/RegisterService.java
@@ -0,0 +1,38 @@
+package pl.zawadzki.myplaces.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.zawadzki.myplaces.dto.LoginAndRegisterRequest;
+import pl.zawadzki.myplaces.exception.UserAlreadyExistsException;
+import pl.zawadzki.myplaces.model.User;
+import pl.zawadzki.myplaces.repository.UserRepository;
+
+@Service
+public class RegisterService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public RegisterService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public boolean isSignUpCompleted(LoginAndRegisterRequest request) {
+        if (doesUserExist(request)) {
+            throw new UserAlreadyExistsException("User " + request.getUsername() + " already exists!");
+        }
+        User newUser = new User(request.getUsername(), passwordEncoder.encode(request.getPassword()));
+        userRepository.save(newUser);
+        return true;
+    }
+
+
+    /**
+     * Checks if User already exists
+     */
+    private boolean doesUserExist(LoginAndRegisterRequest request) {
+        return userRepository.getUserByUsername(request.getUsername()).isPresent();
+    }
+}
