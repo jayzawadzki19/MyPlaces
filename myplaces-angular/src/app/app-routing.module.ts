101ebb41f854a16b8d812fdd1e7d1de02d8fa51f
Jay <jzawadzki19@gmail.com>
2021-01-22 02:06:05 +0100

    config files update

--- myplaces-angular/src/app/app-routing.module.ts
+++ myplaces-angular/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MapComponent} from './modules/pages/map/map.component';
+import {LoginComponent} from './modules/pages/login/login.component';
+import {RegisterComponent} from './modules/pages/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MapComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myplaces-angular/src/app/app.module.ts
+++ myplaces-angular/src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {NavbarComponent} from './modules/components/navbar/navbar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {MapComponent} from './modules/pages/map/map.component';
+import {LoginComponent} from './modules/pages/login/login.component';
+import {RegisterComponent} from './modules/pages/register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {httpInterceptorProviders} from './core/service/interceptor';
+import {HttpClientModule} from '@angular/common/http';
+import {LocalStorageService, NgxWebstorageModule} from 'ngx-webstorage';
+import { MarkerDetailsComponent } from './modules/components/marker-details/marker-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MapComponent,
+    LoginComponent,
+    RegisterComponent,
+    MarkerDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LeafletModule,
+    NgbModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxWebstorageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
